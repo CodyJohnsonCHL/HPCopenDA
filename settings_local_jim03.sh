96778099b71cd07b85a95a664772bfa97741217e
Cody Lee Johnson <cody@jim.erdc.hpc.mil>
2021-11-23 17:21:22 -0600

    examples are working

---
+++ settings_local_jim03.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#
+
+# -- do not change anything below this line
+# OpenDA
+export PATH=$OPENDADIR:$PATH
+export OPENDA_NATIVE=SYSTEM        
+export OPENDALIB=$OPENDADIR/$OPENDA_NATIVE/lib
+export eclipse_exe=eclipse
--- setup_openda.sh
+++ setup_openda.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
+export PATH="/p/home/cody/jdk-17.0.1/bin:${PATH}"
 export OPENDAVER="3.0.3"
-export OPENDADIR="/p/home/cody/openda_${OPENDAVER}"
+export OPENDADIR="/p/home/cody/openda_${OPENDAVER}/bin"
 
 
