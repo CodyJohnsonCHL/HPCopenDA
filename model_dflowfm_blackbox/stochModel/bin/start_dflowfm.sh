9b60860072106c605aaa7d191106565027422542
Cody Johnson <mail@codyjohnson.xyz>
2021-11-24 11:13:49 -0600

    initial modification

--- model_dflowfm_blackbox/stochModel/bin/start_dflowfm.sh
+++ model_dflowfm_blackbox/stochModel/bin/start_dflowfm.sh
@@ -1,4 +1,16 @@
 #! /bin/sh
 #
-module load dflowfm
-run_dflowfm.sh --autostartstop $1
+
+# Modules
+module purge
+module load cmake costinit valgrind netcdf-c/intel netcdf-cxx/intel netcdf-fortran/intel compiler/intel/18.0.4 mpi/intelmpi/18.0.4
+
+# Environment
+export D3D_HOME="/p/home/cody/Delft_FM/v67088-1.2.4"
+export LD_LIBRARY_PATH=${D3D_HOME}/src/lib:${D3D_HOME}/petsc-3.13.0/linux-intel/lib:${LD_LIBRARY_PATH}
+
+MDU=$1
+
+# Execute
+cd ${RUN_DIR}
+mpirun -np 36 ${D3D_HOME}/src/bin/dflowfm --autostartstop ${MDU}
--- setup_openda.sh
+++ setup_openda.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
 # Modules
-module purge
-module load cmake costinit valgrind netcdf-c/intel netcdf-cxx/intel netcdf-fortran/intel compiler/intel/18.0.4 mpi/intelmpi/18.0.4
+#module purge
+#module load cmake costinit valgrind netcdf-c/intel netcdf-cxx/intel netcdf-fortran/intel compiler/intel/18.0.4 mpi/intelmpi/18.0.4
 
 # Environment
-export D3D_HOME="/p/home/cody/Delft_FM/v67088-1.2.4"
-export LD_LIBRARY_PATH=${D3D_HOME}/src/lib:${D3D_HOME}/petsc-3.13.0/linux-intel/lib:${LD_LIBRARY_PATH}
-export PATH="${D3D_HOME}/src/bin:${PATH}"
+#export D3D_HOME="/p/home/cody/Delft_FM/v67088-1.2.4"
+#export LD_LIBRARY_PATH=${D3D_HOME}/src/lib:${D3D_HOME}/petsc-3.13.0/linux-intel/lib:${LD_LIBRARY_PATH}
+#export PATH="${D3D_HOME}/src/bin:${PATH}"
 
 export PATH="/p/home/cody/jdk-17.0.1/bin:${PATH}"
 export OPENDAVER="3.0.3"
