0c3f1d8e077fddff508c1db47b62eb7e3dd30fd8
Cody Lee Johnson <cody@jim.erdc.hpc.mil>
2021-11-29 14:12:35 -0600

    mpi example model running

--- model_dflowfm_blackbox/stochModel/bin/start_dflowfm.sh
+++ model_dflowfm_blackbox/stochModel/bin/start_dflowfm.sh
@@ -9,8 +9,6 @@ module load cmake costinit valgrind netcdf-c/intel netcdf-cxx/intel netcdf-fortr
 export D3D_HOME="/p/home/cody/Delft_FM/v67088-1.2.4"
 export LD_LIBRARY_PATH=${D3D_HOME}/src/lib:${D3D_HOME}/petsc-3.13.0/linux-intel/lib:${LD_LIBRARY_PATH}
 
-MDU=$1
-
 # Execute
-cd ${RUN_DIR}
-mpirun -np 36 ${D3D_HOME}/src/bin/dflowfm --autostartstop ${MDU}
+echo $(pwd)
+mpirun -np 36 ${D3D_HOME}/src/bin/dflowfm --autostartstop $1
