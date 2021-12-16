f257054dba3c8c67918f5097669203670203298c
Cody Johnson <mail@codyjohnson.xyz>
2021-12-16 13:42:45 -0600

    pbs

--- tidal_calibration/stochModel/bin/start_dflowfm.sh
+++ tidal_calibration/stochModel/bin/start_dflowfm.sh
@@ -1,13 +1,4 @@
 #! /bin/sh
 #
 
-# Modules
-module purge
-module load cmake costinit valgrind netcdf-c/intel netcdf-cxx/intel netcdf-fortran/intel compiler/intel/18.0.4 mpi/intelmpi/18.0.4
-
-# Environment
-export D3D_HOME="/p/home/cody/Delft_FM/v67088-1.2.4"
-export LD_LIBRARY_PATH=${D3D_HOME}/src/lib:${D3D_HOME}/petsc-3.13.0/linux-intel/lib:${LD_LIBRARY_PATH}
-
-# Execute
-mpirun -np 36 ${D3D_HOME}/src/bin/dflowfm --autostartstop $1
+qsub start_d3d.pbs
